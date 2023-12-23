a922a7b0f7e4657dfb1e4bad6fed897d06483262
Jansinivetha <jansinivetha.s@3edge.in>
2023-12-23 12:07:56 +0530

    firstcommit

---
+++ new.java
@@ -0,0 +1,10 @@
+import java.util.*:
+public class Main{
+    public static void main(String[] args)
+    {
+        int a=5;
+        int b=6;
+        int c=a*b;
+        System.out.print(c);
+    }
+}
